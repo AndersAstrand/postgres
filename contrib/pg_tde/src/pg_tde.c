aee6b6fdbcba060190e79b2a661b587df6a37095
Andreas Karlsson <andreas.karlsson@percona.com>
2025-05-02 19:43:58 +0200

    Do not use our shmem utils in the key provider code
    Since the only thing our key provider code does with shared memory is
    look up a LWLock tranche it is quite a waste of lines of codes to use
    our own layer on top of the PostgreSQL shared memory.

--- contrib/pg_tde/src/pg_tde.c
+++ contrib/pg_tde/src/pg_tde.c
@@ -72,6 +72,7 @@ tde_shmem_startup(void)
 		prev_shmem_startup_hook();
 
 	TdeShmemInit();
+	InitializeKeyProviderInfo();
 	TDEXLogShmemInit();
 	TDEXLogSmgrInit();
 }
@@ -98,7 +99,6 @@ _PG_init(void)
 	TdeGucInit();
 	TdeEventCaptureInit();
 	InitializePrincipalKeyInfo();
-	InitializeKeyProviderInfo();
 	InstallFileKeyring();
 	InstallVaultV2Keyring();
 	InstallKmipKeyring();
